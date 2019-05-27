78d752f12d499a2de0e3dc31d79a0dd74c89928c
rahuldevaragudde <32443524+rahuldevaragudde@users.noreply.github.com>
2019-05-27 14:26:02 -0700

    Update server.py

--- server.py
+++ server.py
@@ -6,4 +6,4 @@ IPAddr = socket.gethostbyname(hostname)  #to get computers ip address
 print("Your Computer Name is:" + hostname)    
 print("Your Computer IP Address is:" + IPAddr)
 print ("starting server ......")
-subprocess.run(["/usr/local/bin/iperf3","-s","-i","1","-1","-p","5002"])
+subprocess.run(["/usr/local/bin/iperf3","-s","-i","1","-p"])
