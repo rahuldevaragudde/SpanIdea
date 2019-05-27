e92f7474a3748ae621b82f627bee649f8a6d3998
rahuldevaragudde <32443524+rahuldevaragudde@users.noreply.github.com>
2019-05-27 14:02:07 -0700

    Create server.py

---
+++ server.py
@@ -0,0 +1,9 @@
+#!/usr/local/bin/python3
+import subprocess
+import socket    
+hostname = socket.gethostname() #to get the computers hostname    
+IPAddr = socket.gethostbyname(hostname)  #to get computers ip address  
+print("Your Computer Name is:" + hostname)    
+print("Your Computer IP Address is:" + IPAddr)
+print ("starting server ......")
+subprocess.run(["/usr/local/bin/iperf3","-s","-i","1","-1","-p","5002"])
