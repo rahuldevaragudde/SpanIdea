fddc761e1c9c1c291d2973ddaa65f732fda31352
saikeerthanagajula <51098329+saikeerthanagajula@users.noreply.github.com>
2019-05-28 10:53:26 -0700

    minor_version_forloop

--- for.py
+++ for.py
@@ -1,5 +1,11 @@
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+'''a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 x=a
 for x in a: 
     if x <= 5:
-        print(f'this value of this index is less than 5 and it is {x}')
+        print(f'this value of this index is less than 5 and it is {x}')'''
+
+for number in range(1,11,3):
+    print(number)
+for letter in "abcd":
+    print(letter)
+
