a0f91f5e70f5267f4013083fad7c1892f899e44d
Sai Keerthana Gajula <gsaikeerthana12@gmail.com>
2019-05-29 10:03:10 -0700

    practice

---
+++ age.py
@@ -0,0 +1,5 @@
+#!/usr/local/bin/python3
+name = input("What is your name: ")
+age = int(input("How old are you: "))
+year = str((2019 - age)+100)
+print(name + " will be 100 years old in the year " + year)
