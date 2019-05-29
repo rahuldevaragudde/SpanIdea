5edf32f1cde0fc3f927f5678f4fc96063c55fcb1
AchuAnnaAntony <31343019+AchuAnnaAntony@users.noreply.github.com>
2019-05-29 12:28:33 -0700

    Update client.py

--- client.py
+++ client.py
@@ -3,4 +3,4 @@ import subprocess
 import sys
 server=sys.argv[1]
 #print("connecting to ", server)
-subprocess.run(["/usr/local/bin/iperf3","-c", server, "-i", "1", "-t", "10"]) 
+subprocess.run(["/usr/bin/iperf3","-c", server, "-i", "1", "-t", "10"]) 
