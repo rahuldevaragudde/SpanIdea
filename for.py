6bc37318810892735fa141757cbeecc10f2347c7
Sai Keerthana Gajula <gsaikeerthana12@gmail.com>
2019-05-28 21:51:28 -0700

    correctcode

--- for.py
+++ for.py
@@ -1,10 +1,6 @@
-# '''a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-# x=a
-# for x in a:
-#     if x <= 5:
-#         print(f'this value of this index is less than 5 and it is {x}')'''
-
-for number in range(1,11,3):
-    print(number)
-for letter in "abcd":
-    print(letter)
+#!/usr/local/bin/python3
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+x=a
+for x in a:
+    if x <= 5:
+        print(f'this value of this index is less than 5 and it is {x}')
